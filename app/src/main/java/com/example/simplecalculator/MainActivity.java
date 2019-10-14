7e71379d3e7f855b229320dd6ff252ac983a08c1
iamparag <mandalpartha.14@gmail.com>
2019-10-15 00:06:32 +0600

    numeric buttons function added

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //Declare all the buttons and text veiw
+    //Declare all the buttons and text view
     private Button zero, one, two, three, four, five, six, seven, eight, nine, dot;
     private Button add, substract, multiply, divide, clearAll, equal;
     private EditText values;
@@ -17,6 +18,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupButtons();
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "0");
+            }
+        });
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "1");
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "8");
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + "9");
+            }
+        });
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values.setText(values.getText().toString() + ".");
+            }
+        });
+
+
     }
     private void setupButtons(){
         //Setup All the buttons and Text views
@@ -30,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
         seven = findViewById(R.id.buttonSeven);
         eight = findViewById(R.id.buttonEight);
         nine = findViewById(R.id.buttonNine);
+        dot = findViewById(R.id.buttonDot);
         add = findViewById(R.id.buttonAdd);
         substract = findViewById(R.id.buttonSubtract);
         multiply = findViewById(R.id.buttonMultiply);
